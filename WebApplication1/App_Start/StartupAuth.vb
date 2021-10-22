e504a0b9bcaa4788b8f3d59b9f6dd8a39d4c4438
a84195472 <a84195472@A84195472A>
2021-10-22 18:32:53 +0300

    Add project files.

---
+++ WebApplication1/App_Start/StartupAuth.vb
@@ -0,0 +1,59 @@
+﻿Imports System.IdentityModel.Claims
+Imports System.Linq
+Imports System.Threading.Tasks
+Imports Microsoft.Owin.Extensions
+Imports Microsoft.Owin.Security
+Imports Microsoft.Owin.Security.Cookies
+Imports Microsoft.Owin.Security.OpenIdConnect
+Imports Owin
+
+Partial Public Class Startup
+    Private Shared clientId As String = ConfigurationManager.AppSettings("ida:ClientId")
+    Private Shared aadInstance As String = EnsureTrailingSlash(ConfigurationManager.AppSettings("ida:AADInstance"))
+
+    Private Shared authority As String = aadInstance & "common"
+
+    Public Sub ConfigureAuth(app As IAppBuilder)
+        app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType)
+
+        app.UseCookieAuthentication(New CookieAuthenticationOptions())
+
+        ' instead of using the default validation (validating against a single issuer value, as we do in line of business apps), 
+        ' we inject our own multitenant validation logic
+        app.UseOpenIdConnectAuthentication(New OpenIdConnectAuthenticationOptions() With {
+              .ClientId = clientId,
+              .Authority = authority,
+              .TokenValidationParameters = New Microsoft.IdentityModel.Tokens.TokenValidationParameters() With {
+                  .ValidateIssuer = False
+              },
+              .Notifications = New OpenIdConnectAuthenticationNotifications() With
+              {
+                  .SecurityTokenValidated = Function(context)
+                                                'If your authentication logic is based on users
+                                                Return Task.FromResult(0)
+                                            End Function,
+                  .AuthenticationFailed = Function(context)
+                                              ' Pass in the context back to the app
+                                              context.HandleResponse()
+                                              ' Suppress the exception
+                                              Return Task.FromResult(0)
+                                          End Function
+              }
+        })
+
+        'This makes any middleware defined above this line run before the Authorization rule is applied in web.config
+        app.UseStageMarker(PipelineStage.Authenticate)
+    End Sub
+
+    Private Shared Function EnsureTrailingSlash(ByRef value As String) As String
+        If (IsNothing(value)) Then
+            value = String.Empty
+        End If
+
+        If (Not value.EndsWith("/", StringComparison.Ordinal)) Then
+            Return value & "/"
+        End If
+
+        Return value
+    End Function
+End Class
---
+++ WebApplication1/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="WebApplication1._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h1>Huawei Document Management</h1>
+        <p class="lead">Document management system (DMS) is a system used to receive, track, manage and store documents and reduce paper.</p>
+        <p><a href="http://www.asp.net" class="btn btn-primary btn-lg">Learn more &raquo;</a></p>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            <h2>Getting Started</h2>
+            <p>
+                ASP.NET Web Forms lets you build dynamic websites using a familiar drag-and-drop, event-driven model.
+            A design surface and hundreds of controls and components let you rapidly build sophisticated, powerful UI-driven sites with data access.
+            </p>
+            <p>
+                <a class="btn btn-default" href="https://go.microsoft.com/fwlink/?LinkId=301948">Learn more &raquo;</a>
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>Store your Document & Repositries</h2>
+            <p>
+                Store your repositries with Gut-Hub, Bit-Bucket, GitLab and Huawei Software Repostiries.
+            </p>
+            <p>
+                <a class="btn btn-default" href="https://www.huaweicloud.com/intl/en-us/product/swr.html">Learn more &raquo;</a>
+            </p>
+        </div>
+        <div class="col-md-4">
+            <h2>Web Application and Infra Hosting</h2>
+            <p>
+                You can easily find a web hosting company that offers the right mix of features and price for your applications.
+            </p>
+            <p>
+                <a class="btn btn-default" href="https://auth.huaweicloud.com/authui/login.html?locale=en-us&service=https%3A%2F%2Fwww.huaweicloud.com%2Fintl%2Fen-us%2F%3Fned#/login">Learn more &raquo;</a>
+            </p>
+        </div>
+    </div>
+
+</asp:Content>
